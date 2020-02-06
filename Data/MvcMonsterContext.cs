3537a9092d4ab7f0470de48e726f552cca616b66
LeoKnox <LeoKnox@users.noreply.github.com>
2020-02-05 20:52:46 -0700

    Add files via upload

---
+++ Data/MvcMonsterContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcMonster.Models;
+
+namespace MvcMonster.Data
+{
+    public class MvcMonsterContext : DbContext
+    {
+        public MvcMonsterContext(DbContextOptions<MvcMonsterContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Monster> Monster { get; set; }
+    }
+}
