b0c41dd14f047893061effaf687e78dd4cab58c1
LeoKnox <LeoKnox@users.noreply.github.com>
2020-02-05 20:25:44 -0700

    Add files via upload

---
+++ Models/Monster.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMonster.Models
+{
+    public class Monster
+    {
+        public int Id { get; set; }
+        public string Called { get; set; }
+        public string Type { get; set; }
+        public int Hp { get; set; }
+        public int Damage { get; set; }
+    }
+}
