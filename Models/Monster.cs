466e8dc0febb78144148daafecfb7a6a851b7094
LeoKnox <LeoKnox@users.noreply.github.com>
2020-02-06 23:43:35 -0700

    Add files via upload

--- Models/Monster.cs
+++ Models/Monster.cs
@@ -9,11 +9,20 @@ namespace MvcMonster.Models
     public class Monster
     {
         public int Id { get; set; }
+        [StringLength(60, MinimumLength = 3)]
         public string Called { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [Required]
         public string Type { get; set; }
         [Display(Name = "Max HP")]
+        [Range(10, 300)]
+        [Required]
         public int Hp { get; set; }
+        [Range(10, 300)]
+        [Required]
         public int Damage { get; set; }
+        [Range(1, 100)]
+        [Required]
         public int Defense { get; set; }
     }
 }
